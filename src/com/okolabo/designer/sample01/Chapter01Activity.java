cc23f0d25add6880c26acc3a8bdd0e0bbb8bd6c7
patorash <chariderpato@gmail.com>
2011-09-27 17:30:56 +0900

    First commit

---
+++ src/com/okolabo/designer/sample01/Chapter01Activity.java
@@ -0,0 +1,62 @@
+package com.okolabo.designer.sample01;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class Chapter01Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chapter01);
+        setTitle(R.string.title_chapter01);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch(item.getItemId()) {
+            case R.id.menu_chapter01:
+                intent = new Intent(this, Chapter01Activity.class);
+                startActivity(intent);
+                break;
+                
+            case R.id.menu_chapter02:
+                intent = new Intent(this, Chapter02Activity.class);
+                startActivity(intent);
+                break;
+                
+            case R.id.menu_chapter03:
+                intent = new Intent(this, Chapter03Activity.class);
+                startActivity(intent);
+                break;
+                
+            case R.id.menu_chapter04:
+                intent = new Intent(this, Chapter04Activity.class);
+                startActivity(intent);
+                break;
+                
+            case R.id.menu_chapter05:
+                intent = new Intent(this, Chapter05Activity.class);
+                startActivity(intent);
+                break;
+                
+            case R.id.menu_chapter06:
+                intent = new Intent(this, Chapter06Activity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
+}
